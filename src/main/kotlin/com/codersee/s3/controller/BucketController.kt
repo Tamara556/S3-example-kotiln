36a72f5e8b186fce2d874b17770d2411a1d4e4a1
Tamara556 <murazyan.tamara5566@gmail.com>
2025-10-25 22:10:09 +0400

    s3

--- src/main/kotlin/com/codersee/s3/controller/BucketController.kt
+++ src/main/kotlin/com/codersee/s3/controller/BucketController.kt
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import kotlin.text.Charsets.UTF_8
 
 typealias PutObjectRequestBody = software.amazon.awssdk.core.sync.RequestBody
 
@@ -48,6 +51,44 @@ class BucketController(
         val fileContent = PutObjectRequestBody.fromString(objectRequest.content)
 
         s3Client.putObject(createObjectRequest, fileContent)
+    }
+
+    @GetMapping("/{bucketName}/objects")
+    fun listObjects(@PathVariable bucketName: String): List<String> {
+        val listObjectsRequest = ListObjectsRequest.builder()
+            .bucket(bucketName)
+            .build()
+
+        val response = try {
+            s3Client.listObjects(listObjectsRequest)
+        } catch (ex: Exception) {
+            println("Error listing objects in bucket $bucketName: ${ex.message}")
+            return emptyList()
+        }
+
+        val keys = response.contents().mapNotNull { obj ->
+            val key = obj.key()
+            if (key == null) println("Found null key in bucket $bucketName. Skipping...")
+            key
+        }
+
+        println("Keys found in bucket '$bucketName': $keys")
+        return keys
+    }
+
+    @GetMapping("/{bucketName}/objects/{objectName:.+}")
+    fun getObject(
+        @PathVariable bucketName: String,
+        @PathVariable objectName: String
+    ): String {
+        val getObjectRequest = GetObjectRequest.builder()
+            .bucket(bucketName)
+            .key(objectName)
+            .build()
+
+        s3Client.getObject(getObjectRequest).use {
+            return it.readAllBytes().toString(Charsets.UTF_8)
+        }
     }
 
     data class BucketRequest(val bucketName: String)
